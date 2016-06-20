0fd86fcc47012564c185d73acf4f55f96b7fc7cb
elvinLong <970099734@qq.com>
2016-06-20 18:49:35 +0800

    no message

--- Home.js
+++ Home.js
@@ -5,3 +5,24 @@ export default class Home extends Component {
     return <div>首页1</div>
   }
 }
+
+function Foo() {
+	this.value = 12;
+}
+
+Foo.prototype = {
+	method: function() {}
+}
+
+function Bar() {
+
+}
+
+Bar.prototype = new Foo()
+
+Bar.prototype.foo = "hello world"
+
+Bar.prototype.constructor = Bar
+
+let test = new Bar()
+console.log(test.method)
--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,11 @@ module.exports = {
       {test: /\.css$/, loader: 'style!css'},
       {test: /\.jsx?$/, loader: 'babel?cacheDirectory=.cache', exclude: /node_modules/},
       {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: 'babel',
-              query: {presets:[ 'es2015', 'react', 'stage-0' ]}
-            },
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {presets:[ 'es2015', 'react', 'stage-0' ]}
+      },
     ]
   },
   plugins: [
